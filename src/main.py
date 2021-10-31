bd89d5d53a47b4d5ed78bc2e9df1d7f2aef4d19b
Elin <982467922@qq.com>
2021-10-31 13:05:49 +0800

    新增报告自动生成

--- src/main.py
+++ src/main.py
@@ -6,7 +6,12 @@ import os
 import pandas as pd
 from datetime import date, datetime
 import subprocess
+from docxtpl import DocxTemplate,InlineImage
+from docx.shared import Cm,Inches,Mm,Emu
+import plotly as py
+import plotly.graph_objs as go
 print("读取文件目录中....")
+doc = DocxTemplate('../template/审阅报告模板.docx')
 start_time = datetime.now()
 root = os.walk('../judge_files')
 files_list = []
@@ -16,7 +21,6 @@ run_result = []
 judge_time = []
 files_info = []
 run_output = []
-output_withError = input("是否输出运行报错？")
 print('审阅中...')
 for i in files_list[0]:
     single_files = os.system(f"python ../judge_files/{i}")
@@ -38,6 +42,58 @@ df['代码内容'] = files_info
 df['是否存在抄袭嫌疑'] = df['代码内容'].duplicated(keep=False).values
 df['是否存在抄袭嫌疑'] = df['是否存在抄袭嫌疑'].apply(lambda x:'存在嫌疑' if x == True else '不存在')
 df.to_csv(f'../results/审阅结果.csv',index=True)
+homework_counts = df.shape[0]
+run_success_percent = float(df[df['运行结果'] == "运行成功"].shape[0] / df[df['运行结果'] == "运行失败"].shape[0] * 100)
+if run_success_percent >= 60:
+    performace = "良好"
+else:
+    performace = "欠佳"
+labels = df["运行结果"].groupby(df["运行结果"]).count().index.tolist()
+values = df["运行结果"].groupby(df["运行结果"]).count().values.tolist()
+trace = [go.Pie(
+        labels = labels, 
+        values = values, 
+        hole =  0.7,
+        hoverinfo = "label + value")]
+layout = go.Layout(
+    title = "当前作业完成率",
+    title_x = 0.5
+)
+fig = go.Figure(data=trace,layout=layout)
+fig.write_image('../images/success_percent_images.jpeg')
+success_percent_images = InlineImage(doc,'../images/success_percent_images.jpeg',Cm(15))
+duplicate_counts = df[df['是否存在抄袭嫌疑'] == "存在嫌疑"].shape[0]
+duplicate_files = df[df['是否存在抄袭嫌疑'] == "存在嫌疑"]['文件名']
+duplicate_percent = float(len(duplicate_files) / df.shape[0] * 100)
+labels2 = df["是否存在抄袭嫌疑"].groupby(df["是否存在抄袭嫌疑"]).count().index.tolist()
+values2 = df["是否存在抄袭嫌疑"].groupby(df["是否存在抄袭嫌疑"]).count().values.tolist()
+trace2 = [go.Pie(
+        labels = labels2, 
+        values = values2, 
+        hole =  0.7,
+        hoverinfo = "label + value")]
+layout2 = go.Layout(
+    title = "当前作业抄袭嫌疑占比图表",
+    title_x = 0.5
+)
+fig2 = go.Figure(data=trace2,layout=layout2)
+fig2.write_image('../images/duplicate_percent_images.jpeg')
+duplicate_percent_images = InlineImage(doc,'../images/duplicate_percent_images.jpeg',Cm(15))
+report_generate_time = str(datetime.now())
+context = {
+    'homework_counts':homework_counts,
+    'run_success_percent':run_success_percent,
+    'performace':performace,
+    'success_percent_images':success_percent_images,
+    'duplicate_counts':duplicate_counts,
+    'duplicate_files':duplicate_files,
+    'duplicate_percent':duplicate_percent,
+    'duplicate_percent_images':duplicate_percent_images,
+    'report_generate_time':report_generate_time
+}
+
+doc.render(context=context)
+doc.save('../results/审阅报告.docx')
 end_time = datetime.now()
 print(f'------------------------------------------------')
-print(f'表已导出至同级目录完成！用时：{end_time-start_time}秒')
+print(f'结果和报告已导出至/results目录下！用时：{end_time-start_time}秒')
